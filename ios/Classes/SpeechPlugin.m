48ddbd2d1477e45744e023cbcf36cb81829f389a
Woody Guo <woody.guo@gmail.com>
2020-02-10 14:25:41 +0800

    Reset audio category to playback after speech regonition

--- ios/Classes/SpeechPlugin.m
+++ ios/Classes/SpeechPlugin.m
@@ -1,30 +1,29 @@
 #import "SpeechPlugin.h"
 #import <AVFoundation/AVFoundation.h>  // 录音
-#import <Speech/Speech.h>    // 语音识别
+#import <Speech/Speech.h>              // 语音识别
 
 @interface SpeechPlugin ()<SFSpeechRecognizerDelegate> {
-  FlutterMethodChannel *_channel;
-  SFSpeechRecognizer *_speechRecognizerEn;
-  SFSpeechRecognizer *_speechRecognizerCn;
-  AVAudioEngine *_audioEngine;
-  SFSpeechAudioBufferRecognitionRequest *_speechAudioBufferRecognitionRequest;
-  SFSpeechRecognitionTask *_speechRecognitionTask;
-  AVAudioRecorder *_recorder;
-  NSTimer *_levelTimer;
+  FlutterMethodChannel* _channel;
+  SFSpeechRecognizer* _speechRecognizerEn;
+  SFSpeechRecognizer* _speechRecognizerCn;
+  AVAudioEngine* _audioEngine;
+  SFSpeechAudioBufferRecognitionRequest* _speechAudioBufferRecognitionRequest;
+  SFSpeechRecognitionTask* _speechRecognitionTask;
+  AVAudioRecorder* _recorder;
+  NSTimer* _levelTimer;
 }
 @end
 
 @implementation SpeechPlugin
 
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
-  FlutterMethodChannel *channel = [FlutterMethodChannel
-      methodChannelWithName:@"speech"
-            binaryMessenger:[registrar messenger]];
-  SpeechPlugin *instance = [[SpeechPlugin alloc] initWithFlutterMethodChannel:channel];
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel =
+      [FlutterMethodChannel methodChannelWithName:@"speech" binaryMessenger:[registrar messenger]];
+  SpeechPlugin* instance = [[SpeechPlugin alloc] initWithFlutterMethodChannel:channel];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
-- (instancetype)initWithFlutterMethodChannel:(FlutterMethodChannel *)channel {
+- (instancetype)initWithFlutterMethodChannel:(FlutterMethodChannel*)channel {
   if (self = [super init]) {
     _channel = channel;
     _speechRecognizerCn =
@@ -36,15 +35,16 @@
   return self;
 }
 
-- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
   } else if ([@"toOpenPermission" isEqualToString:call.method]) {
-      [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
-  }  else if ([@"speech.activate" isEqualToString:call.method]) {
+    [[UIApplication sharedApplication]
+        openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+  } else if ([@"speech.activate" isEqualToString:call.method]) {
     [self activateRecognition:result];
   } else if ([@"speech.start" isEqualToString:call.method]) {
-    [self startRecognition:call.arguments :result];
+    [self startRecognition:call.arguments:result];
   } else if ([@"speech.cancel" isEqualToString:call.method]) {
     [self cancelRecognition:result];
   } else if ([@"speech.stop" isEqualToString:call.method]) {
@@ -56,39 +56,41 @@
 
 // 请求权限
 - (void)activateRecognition:(FlutterResult)result {
-    SFSpeechRecognizerAuthorizationStatus status = [SFSpeechRecognizer authorizationStatus];
-    if (status == SFSpeechRecognizerAuthorizationStatusRestricted || status == SFSpeechRecognizerAuthorizationStatusDenied) {
-        result([NSNumber numberWithBool:NO]);
-    } else if (status == SFSpeechRecognizerAuthorizationStatusAuthorized) {
+  SFSpeechRecognizerAuthorizationStatus status = [SFSpeechRecognizer authorizationStatus];
+  if (status == SFSpeechRecognizerAuthorizationStatusRestricted ||
+      status == SFSpeechRecognizerAuthorizationStatusDenied) {
+    result([NSNumber numberWithBool:NO]);
+  } else if (status == SFSpeechRecognizerAuthorizationStatusAuthorized) {
+    [self activateAuthorization:result];
+  } else {
+    [SFSpeechRecognizer requestAuthorization:^(SFSpeechRecognizerAuthorizationStatus status) {
+      if (status == SFSpeechRecognizerAuthorizationStatusAuthorized) {
         [self activateAuthorization:result];
-    } else {
-        [SFSpeechRecognizer requestAuthorization:^(SFSpeechRecognizerAuthorizationStatus status) {
-            if (status == SFSpeechRecognizerAuthorizationStatusAuthorized) {
-                 [self activateAuthorization:result];
-            } else{
-            result(nil);
-            }
-        }];
-    }
+      } else {
+        result(nil);
+      }
+    }];
+  }
 }
 
-// 申请麦克风权限 
+// 申请麦克风权限
 - (void)activateAuthorization:(FlutterResult)result {
-    AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeAudio];
-    if (status == AVAuthorizationStatusRestricted || status == AVAuthorizationStatusDenied) {
-        result([NSNumber numberWithBool:NO]);
-    } else if (status == AVAuthorizationStatusAuthorized) {
-        result([NSNumber numberWithBool:YES]);
-    } else {
-        [AVCaptureDevice requestAccessForMediaType:AVMediaTypeAudio completionHandler:^(BOOL granted) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                result(granted ? [NSNumber numberWithBool:YES] : nil);
-            });
-        }];
-    }
+  AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeAudio];
+  if (status == AVAuthorizationStatusRestricted || status == AVAuthorizationStatusDenied) {
+    result([NSNumber numberWithBool:NO]);
+  } else if (status == AVAuthorizationStatusAuthorized) {
+    result([NSNumber numberWithBool:YES]);
+  } else {
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeAudio
+                             completionHandler:^(BOOL granted) {
+                               dispatch_async(dispatch_get_main_queue(), ^{
+                                 result(granted ? [NSNumber numberWithBool:YES] : nil);
+                               });
+                             }];
+  }
 }
 
-- (void)startRecognition:(NSString *)local :(FlutterResult)result {
+- (void)startRecognition:(NSString*)local:(FlutterResult)result {
   NSLog(@"startRecognition...");
   if (_audioEngine.isRunning) {
     [_audioEngine stop];
@@ -100,7 +102,7 @@
     bool resultTemp = YES;
     @try {
       [self start:local];
-    } @catch (NSException *exception) {
+    } @catch (NSException* exception) {
       resultTemp = NO;
       NSLog(@"%@", exception);
     } @finally {
@@ -119,16 +121,22 @@
     }
   }
 
-    [self recorderSoundEnd];
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    NSError * error = nil;
-    [session setCategory:AVAudioSessionCategoryAmbient withOptions:AVAudioSessionCategoryOptionDuckOthers error:&error];
-    if (error) {
-        NSLog(@"%@",error);
-    }
-    [session setActive:YES withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:nil];
+  [self recorderSoundEnd];
+  AVAudioSession* session = [AVAudioSession sharedInstance];
+  NSError* error = nil;
+  [session setCategory:AVAudioSessionCategoryPlayback
+           withOptions:AVAudioSessionCategoryOptionDuckOthers
+                 error:&error];
+  if (error) {
+    NSLog(@"%@", error);
+  }
+  [session setActive:YES
+         withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation
+               error:nil];
 
-    [session setActive:NO withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:nil];
+  [session setActive:NO
+         withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation
+               error:nil];
 }
 
 - (void)stopRecognition:(FlutterResult)result {
@@ -143,18 +151,19 @@
   result(@(NO));
 }
 
-- (void)start:(NSString *)local {
-
+- (void)start:(NSString*)local {
   [self cancelRecognition:nil];
 
-  AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+  AVAudioSession* audioSession = [AVAudioSession sharedInstance];
   @try {
     [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord
                          mode:AVAudioSessionModeMeasurement
-                      options:AVAudioSessionCategoryOptionDuckOthers error:nil];
+                      options:AVAudioSessionCategoryOptionDuckOthers
+                        error:nil];
     [audioSession setActive:YES
-                withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:nil];
-  } @catch (NSException *exception) {
+                withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation
+                      error:nil];
+  } @catch (NSException* exception) {
     NSLog(@"%@", exception);
   }
 
@@ -166,41 +175,39 @@
 
   _speechAudioBufferRecognitionRequest.shouldReportPartialResults = YES;
 
-  AVAudioInputNode *inputNode = _audioEngine.inputNode;
-  SFSpeechRecognizer *speechRecognizer = [self getRecognizer:local];
-  _speechRecognitionTask =
-      [speechRecognizer
-          recognitionTaskWithRequest:_speechAudioBufferRecognitionRequest
-                       resultHandler:^(SFSpeechRecognitionResult *_Nullable result,
-                                       NSError *_Nullable error) {
-                         NSLog(@"is final: %d  result: %@",
-                               result.isFinal,
-                               result.bestTranscription.formattedString);
-                         bool isFinal = false;
-                         if (result) {
-                           [_channel invokeMethod:@"speech.onSpeech"
-                                        arguments:result.bestTranscription.formattedString];
-                           isFinal = result.isFinal;
-                           if (isFinal) {
-                             [_channel invokeMethod:@"speech.onRecognitionComplete"
-                                          arguments:result.bestTranscription
-                                 .formattedString];
+  AVAudioInputNode* inputNode = _audioEngine.inputNode;
+  SFSpeechRecognizer* speechRecognizer = [self getRecognizer:local];
+  _speechRecognitionTask = [speechRecognizer
+      recognitionTaskWithRequest:_speechAudioBufferRecognitionRequest
+                   resultHandler:^(SFSpeechRecognitionResult* _Nullable result,
+                                   NSError* _Nullable error) {
+                     NSLog(@"is final: %d  result: %@", result.isFinal,
+                           result.bestTranscription.formattedString);
+                     bool isFinal = false;
+                     if (result) {
+                       [_channel invokeMethod:@"speech.onSpeech"
+                                    arguments:result.bestTranscription.formattedString];
+                       isFinal = result.isFinal;
+                       if (isFinal) {
+                         [_channel invokeMethod:@"speech.onRecognitionComplete"
+                                      arguments:result.bestTranscription.formattedString];
+                       }
+                     }
+                     if (error) {
+                       [_channel invokeMethod:@"speech.onError" arguments:nil];
+                     }
 
-                           }
-                         }
-                         if (error) {
-                             [_channel invokeMethod:@"speech.onError" arguments:nil];
-                         }
+                   }];
 
-                       }];
-
-  AVAudioFormat *recordingFormat = [inputNode outputFormatForBus:0];
+  AVAudioFormat* recordingFormat = [inputNode outputFormatForBus:0];
 
   [inputNode removeTapOnBus:0];
-  [inputNode installTapOnBus:0 bufferSize:1024 format:recordingFormat block:^(AVAudioPCMBuffer *_Nonnull
-  buffer, AVAudioTime *_Nonnull when) {
-    [_speechAudioBufferRecognitionRequest appendAudioPCMBuffer:buffer];
-  }];
+  [inputNode installTapOnBus:0
+                  bufferSize:1024
+                      format:recordingFormat
+                       block:^(AVAudioPCMBuffer* _Nonnull buffer, AVAudioTime* _Nonnull when) {
+                         [_speechAudioBufferRecognitionRequest appendAudioPCMBuffer:buffer];
+                       }];
 
   [_audioEngine prepare];
   @try {
@@ -208,14 +215,14 @@
 
     [self recorderSoundStart];
 
-  } @catch (NSException *exception) {
+  } @catch (NSException* exception) {
     NSLog(@"%@", exception);
   }
 
   [_channel invokeMethod:@"speech.onRecognitionStarted" arguments:nil];
 }
 
-- (SFSpeechRecognizer *)getRecognizer:(NSString *)local {
+- (SFSpeechRecognizer*)getRecognizer:(NSString*)local {
   if ([@"en_US" isEqualToString:local]) {
     return _speechRecognizerEn;
   } else {
@@ -226,15 +233,15 @@
 #pragma mark - 录音相关
 - (void)createAudioRecorder {
   // 实例化录音器对象
-  NSError *errorRecord = nil;
+  NSError* errorRecord = nil;
   // 不需要保存录音
-  _recorder =
-      [[AVAudioRecorder alloc] initWithURL:[NSURL fileURLWithPath:@"/dev/null"]
-                                  settings:[self getAudioSetting] error:&errorRecord];
-  _recorder.meteringEnabled = YES; //如果要监控声波则必须设置为YES
+  _recorder = [[AVAudioRecorder alloc] initWithURL:[NSURL fileURLWithPath:@"/dev/null"]
+                                          settings:[self getAudioSetting]
+                                             error:&errorRecord];
+  _recorder.meteringEnabled = YES;  //如果要监控声波则必须设置为YES
 
-    // 准备录音
-    [_recorder prepareToRecord];
+  // 准备录音
+  [_recorder prepareToRecord];
 }
 
 - (void)recorderSoundStart {
@@ -243,16 +250,17 @@
     [_recorder stop];
   }
 
-    // 实例化录音对象
-    [self createAudioRecorder];
+  // 实例化录音对象
+  [self createAudioRecorder];
 
   if (![_recorder isRecording]) {
-
     [_recorder record];
 
-    _levelTimer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector
-    (levelTimerCallback:)                        userInfo:nil repeats:YES];
-
+    _levelTimer = [NSTimer scheduledTimerWithTimeInterval:0.5
+                                                   target:self
+                                                 selector:@selector(levelTimerCallback:)
+                                                 userInfo:nil
+                                                  repeats:YES];
   }
 }
 
@@ -268,11 +276,11 @@
 }
 
 /* 该方法确实会随环境音量变化而变化，但具体分贝值是否准确暂时没有研究 */
-- (void)levelTimerCallback:(NSTimer *)timer {
+- (void)levelTimerCallback:(NSTimer*)timer {
   [_recorder updateMeters];
 
   double level;                // The linear 0.0 .. 1.0 value we need.
-  float minDecibels = -80.0f; // Or use -60dB, which I measured in a silent room.
+  float minDecibels = -80.0f;  // Or use -60dB, which I measured in a silent room.
   float decibels = [_recorder averagePowerForChannel:0];
 
   if (decibels < minDecibels) {
@@ -293,18 +301,19 @@
   [_channel invokeMethod:@"speech.db" arguments:[NSNumber numberWithDouble:level]];
 }
 
-- (NSDictionary *)getAudioSetting {
-  return [NSDictionary dictionaryWithObjectsAndKeys:
-                  [NSNumber numberWithFloat: 44100.0], AVSampleRateKey,
-                  [NSNumber numberWithInt: kAudioFormatAppleLossless], AVFormatIDKey,
-                  [NSNumber numberWithInt: 2], AVNumberOfChannelsKey,
-                  [NSNumber numberWithInt: AVAudioQualityMax], AVEncoderAudioQualityKey,
-                  nil];
+- (NSDictionary*)getAudioSetting {
+  return [NSDictionary
+      dictionaryWithObjectsAndKeys:[NSNumber numberWithFloat:44100.0], AVSampleRateKey,
+                                   [NSNumber numberWithInt:kAudioFormatAppleLossless],
+                                   AVFormatIDKey, [NSNumber numberWithInt:2], AVNumberOfChannelsKey,
+                                   [NSNumber numberWithInt:AVAudioQualityMax],
+                                   AVEncoderAudioQualityKey, nil];
 }
 
 #pragma mark - SFSpeechRecognizerDelegate
 
-- (void)speechRecognizer:(SFSpeechRecognizer *)speechRecognizer availabilityDidChange:(BOOL)available {
+- (void)speechRecognizer:(SFSpeechRecognizer*)speechRecognizer
+    availabilityDidChange:(BOOL)available {
   [_channel invokeMethod:@"speech.onSpeechAvailability" arguments:@(available)];
 }
 
